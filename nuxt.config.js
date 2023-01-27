6ab7633c4f035762cbdb4aab7cb38cee3172b96c
Tamby Mampionona <therihar@etceterum.net>
2023-01-27 17:07:11 +0300

    Modification nom projet

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Portail',
+    title: 'Hackathon',
     htmlAttrs: {
       lang: 'en'
     },
