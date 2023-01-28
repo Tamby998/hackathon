dc9f92bff9da27cbe7d805231138d0c1854a8ff4
Tamby Mampionona <therihar@etceterum.net>
2023-01-28 08:51:01 +0300

    Tic tac

---
+++ pages/divertissementConnecter/wiki/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app" class="bg-white">
+        <div class="container-text text-black">TIC TAC TOE</div>
+            <div class="container-game">
+                <divertissementConnecter-wiki />
+            </div>
+        </div>
+</template>
+  
+  <script>
+  </script>
+  <style>
+
+@import url("https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro&family=Roboto:wght@300&display=swap");
+#app {
+  font-family: "Be Vietnam Pro", sans-serif;
+}
+.container-text {
+  font-size: 35px;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 40px;
+}
+.container-game {
+  text-align: center;
+}
+</style>
