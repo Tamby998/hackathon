08283bcebc9bb5f091e3004fa4a4e9f19c9139cf
Tamby Mampionona <therihar@etceterum.net>
2023-01-28 08:23:39 +0300

    Jeux fleche

---
+++ pages/divertissementConnecter/fleche/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <divertissementConnecter-fleche />
+  </template>
+  
+  <script>
+  </script>
+  
+  <!-- <style>
+
+  </style> -->
+  <!-- <template>
+    <Game/>
+</template>
+
+<script>
+import Game from './components/Game.vue'
+export default {
+  name: 'App',
+  components: {
+    Game
+  }
+}
+</script> -->
+
+<style lang="scss">
+
+.main-app {
+  font-family: 'Gluten', cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 2em auto;
+  padding: 1.225em 0;
+  width: 640px;
+  background-image: linear-gradient( 174.2deg,  rgba(255,244,228,1) 7.1%, rgba(240,246,238,1) 67.4% );
+  border-radius: 10px;
+}
+</style>
